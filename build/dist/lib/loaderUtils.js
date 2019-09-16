cb111099a8f3c296ae87d27c5c29be2274c08613
darkiller <darkbish@hotmail.com>
2019-09-16 15:38:25 +0800

    上传编译好的文件

---
+++ build/dist/lib/loaderUtils.js
@@ -0,0 +1,34 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.getDefaultThemePath = exports.getScssThemePath = undefined;
+
+var _index = require('./index');
+
+var _index2 = _interopRequireDefault(_index);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+/**
+ * Get path to SCSS theme file specified in loader options or through the plugin's constructor.
+ * @param {Object} options - Loader options.
+ * @return {string} Path to SCSS theme file.
+ */
+// eslint-disable-next-line import/prefer-default-export
+const getScssThemePath = exports.getScssThemePath = options => {
+  const scssThemePath = options.scssThemePath || _index2.default.SCSS_THEME_PATH;
+
+  if (!scssThemePath) {
+    throw new Error('Path to an SCSS theme file must be specified through the scssThemePath loader option, ' + "or passed to the plugin's constructor.");
+  }
+
+  return scssThemePath;
+};
+
+const getDefaultThemePath = exports.getDefaultThemePath = options => {
+  const antdThemePath = options.antdThemePath || _index2.default.ANTD_THEME_PATH || 'antd/lib/style/themes/default.less';
+
+  return antdThemePath;
+};
